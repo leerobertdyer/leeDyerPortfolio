a1dadc3abf558b35d59f93a6b6ef19ac7c2958e9
RVleerobertdyer <ldyer@redventures.com>
2025-02-09 16:08:43 -0500

    fix: removing width and height from image component

--- src/components/LinkPhoto/LinkPhoto.tsx
+++ src/components/LinkPhoto/LinkPhoto.tsx
@@ -15,7 +15,7 @@ export default function LinkPhoto(props: LinkPhoto) {
 
     return (
         <Link href={linkto} target={target ?? ""} className="flex justify-center w-[30vw] h-[30vw] min-w-[15rem] min-h-[15rem] overflow-hidden relative">
-            <Image width={455} height={455} priority
+            <Image fill priority
                  className="rounded-md object-cover object-center border-2 border-leeWhite-dark" src={src} alt={alt} />
             <p className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-leeWhite-light text-center p-2 rounded-b-md">
                 {text}
